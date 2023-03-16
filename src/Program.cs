6e388708fc67701e183e314704de7b438c998ab4
Drazen Dotlic <drazen.dotlic@gmail.com>
2023-03-16 14:30:18 +0100

    No need for 128-bit Integer either
    We're not computing anything with it, just need to pack 6 7-bit Integers inside and have comparison and hash code working, which the tuple can do just as well.

--- src/Program.cs
+++ src/Program.cs
@@ -3,50 +3,52 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using static CountDown.Solver;
 
-namespace CountDown {
-    public static class Program {
-        private static (List<long>, long) ParseArgs(string[] args) {
-            var numbers = new List<long>();
-            for (int a = 0; a < 6; ++a)
-                numbers.Add(long.Parse(args[a]));
-            var goal = long.Parse(args[6]);
-            return (numbers, goal);
+namespace CountDown; 
+
+public static class Program {
+    private const int _maxTop = 5;
+    private const int _turns = 128;
+
+    private static (List<long>, long) ParseArgs(string[] args) {
+        var numbers = new List<long>();
+        for (var a = 0; a < 6; ++a) {
+            numbers.Add(long.Parse(args[a]));
         }
 
-        private const int _maxTop = 5;
-        private const int _turns = 10;
+        var goal = long.Parse(args[6]);
+        return (numbers, goal);
+    }
 
-        // few results: using 1 1 4 7 15 50 522 should yield
-        // 50 + (1 + 7) * (4 * 15 - 1) as the best result (and one more)
-        // good stress test: using 7 3 4 5 15 75 785 should yield
-        // 13 results using around 120000 tries
-        public static void Main(string[] args) {
-            var numArgs = args.Length;
-            if (numArgs != 7) {
-                Console.WriteLine("Parameters: n1 n2 n3 n4 n5 n6 n7");
-                Console.WriteLine("n1 to n4 are one digit numbers");
-                Console.WriteLine("n5 is 10, 15, or 20");
-                Console.WriteLine("n6 is 25, 50, 75 or 100");
-                Console.WriteLine("n7 is a three digit number to arrive at");
-                return;
-            }
+    // few results: using 1 1 4 7 15 50 522 should yield
+    // 50 + (1 + 7) * (4 * 15 - 1) as the best result (and one more)
+    // good stress test: using 7 3 4 5 15 75 785 should yield
+    // 13 results using around 120000 tries
+    public static void Main(string[] args) {
+        var numArgs = args.Length;
+        if (numArgs != 7) {
+            Console.WriteLine("Parameters: n1 n2 n3 n4 n5 n6 n7");
+            Console.WriteLine("n1 to n4 are one digit numbers");
+            Console.WriteLine("n5 is 10, 15, or 20");
+            Console.WriteLine("n6 is 25, 50, 75 or 100");
+            Console.WriteLine("n7 is a three digit number to arrive at");
+            return;
+        }
 
-            var (numbers, goal) = ParseArgs(args);
-            var time = Stopwatch.StartNew();
-            for (int p = 0; p < _turns; ++p) {
-                Solve(numbers, goal);
-            }
+        var (numbers, goal) = ParseArgs(args);
+        var time = Stopwatch.StartNew();
+        for (var p = 0; p < _turns; ++p) {
+            Solve(numbers, goal);
+        }
 
-            time.Stop();
-            var numResults = Results.Count;
-            var top = Math.Min(numResults, _maxTop);
-            var elapsed = Math.Round(time.ElapsedMilliseconds / (double)_turns, 2);
-            Console.WriteLine(
-                $"Found {numResults} results with {_turns} turns in {elapsed} ms; tried {Combinations} combinations.");
-            Console.WriteLine($"Top {_maxTop} results (or less if there aren't as many)");
-            for (int r = 0; r < top; ++r) {
-                Console.WriteLine(Results[r].Format());
-            }
+        time.Stop();
+        var numResults = Results.Count;
+        var top = Math.Min(numResults, _maxTop);
+        var elapsed = Math.Round(time.ElapsedMilliseconds / (double)_turns, 2);
+        Console.WriteLine(
+            $"Found {numResults} results with {_turns} turns in {elapsed} ms; tried {Combinations} combinations.");
+        Console.WriteLine($"Top {_maxTop} results (or less if there aren't as many)");
+        for (var r = 0; r < top; ++r) {
+            Console.WriteLine(Results[r].Format());
         }
     }
 }
--- src/Solver.cs
+++ src/Solver.cs
@@ -1,187 +1,192 @@
 using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Packed = System.ValueTuple<long, long>;
 
-namespace CountDown {
-    public static class Fmt {
-        private static readonly long[] _priority = { 1, 2, 3, 3 };
-        private static readonly string[] _operator = { "+", "-", "*", "/" };
-        private static readonly (string, string) _parens = ("(", ")");
-        private static readonly (string, string) _nothing = (string.Empty, string.Empty);
+namespace CountDown; 
 
-        public static string Format(this Result res) {
-            return res.AsString(Op.Add);
-        }
+public static class Fmt {
+    private static readonly long[] _priority = { 1, 2, 3, 3 };
+    private static readonly string[] _operator = { "+", "-", "*", "/" };
+    private static readonly (string, string) _parens = ("(", ")");
+    private static readonly (string, string) _nothing = (string.Empty, string.Empty);
 
-        private static string AsString(this Result res, Op parentOp) {
-            switch (res) {
-                case AppRes appr:
-                    var op = appr.Op;
-                    int opi = (int)op;
-                    bool useParen = _priority[(int)parentOp] > _priority[opi] ||
-                                    (parentOp == op && op == Op.Sub);
-                    var (start, end) = useParen ? _parens : _nothing;
-                    return
-                        $"{start}{appr.Left.AsString(op)} {_operator[opi]} {appr.Right.AsString(op)}{end}";
+    public static string Format(this Result res) => res.AsString(Op.Add);
 
-                case ValRes valr:
-                    return valr.Total.ToString();
-                default:
-                    throw new ArgumentException("Unknown result");
-            }
-        }
-    }
+    private static string AsString(this Result res, Op parentOp) {
+        switch (res) {
+            case AppRes appr:
+                var op = appr.Op;
+                var opi = (int)op;
+                var useParen = _priority[(int)parentOp] > _priority[opi] ||
+                               (parentOp == op && op == Op.Sub);
+                var (start, end) = useParen ? _parens : _nothing;
+                return
+                    $"{start}{appr.Left.AsString(op)} {_operator[opi]} {appr.Right.AsString(op)}{end}";
 
-    public enum Op { Add, Sub, Mul, Div };
-
-    public abstract class Result {
-        public long Total;
-
-        public abstract int Operations { get; }
-    }
-
-    public class ValRes : Result {
-        public ValRes(long val) {
-            Total = val;
-        }
-
-        public override int Operations { get => 0; }
-    }
-
-    public class AppRes : Result {
-        public readonly Op Op;
-        public readonly Result Left;
-        public readonly Result Right;
-
-        public AppRes(Op op, Result left, Result right) {
-            Op = op;
-            Left = left;
-            Right = right;
-            Apply();
-        }
-
-        public override int Operations { get => 1 + Left.Operations + Right.Operations; }
-
-        private void Apply() {
-            Total = Op switch {
-                Op.Add => Left.Total + Right.Total,
-                Op.Sub => Left.Total - Right.Total,
-                Op.Mul => Left.Total * Right.Total,
-                Op.Div => Left.Total / Right.Total,
-                _ => throw new ArgumentException("Operator not supported")
-            };
+            case ValRes valr:
+                return valr.Total.ToString();
+            default:
+                throw new ArgumentException("Unknown result");
         }
     }
+}
 
-    public static class Solver {
-        private static readonly Op[] _operations = { Op.Add, Op.Sub, Op.Mul, Op.Div };
-        private static int TotalComparer(Result a, Result b) => a.Total.CompareTo(b.Total);
-        private static int OperationsComparer(Result a, Result b) => a.Operations.CompareTo(b.Operations);
+public enum Op { Add, Sub, Mul, Div }
 
-        public static int Combinations { get; private set; }
-        public static List<Result> Results { get; } = new();
+public abstract class Result {
+    public long Total;
 
-        private static readonly HashSet<UInt128> _cache = new(130_000);
+    public abstract int Operations { get; }
+}
 
-        private static readonly Result[] _memory = new Result[16];
-        private static int _offset;
+public class ValRes : Result {
+    public ValRes(long val) => Total = val;
 
-        private static readonly (int, int)[] _packIndices = {
-            (0, 0), (21, 0), (42, 0), (0, 1), (21, 1), (42, 1),
+    public override int Operations => 0;
+}
+
+public class AppRes : Result {
+    public readonly Result Left;
+    public readonly Op Op;
+    public readonly Result Right;
+
+    public AppRes(Op op, Result left, Result right) {
+        Op = op;
+        Left = left;
+        Right = right;
+        Apply();
+    }
+
+    public override int Operations => 1 + Left.Operations + Right.Operations;
+
+    private void Apply() =>
+        Total = Op switch {
+            Op.Add => Left.Total + Right.Total,
+            Op.Sub => Left.Total - Right.Total,
+            Op.Mul => Left.Total * Right.Total,
+            Op.Div => Left.Total / Right.Total,
+            _ => throw new ArgumentException("Operator not supported")
+        };
+}
+
+public static class Solver {
+    private static readonly Op[] _operations = { Op.Add, Op.Sub, Op.Mul, Op.Div };
+
+    private static readonly HashSet<Packed> _cache = new(130_000);
+
+    private static readonly Result[] _memory = new Result[16];
+    private static int _offset;
+
+    private static readonly (int, int)[] _packIndices = { (0, 0), (21, 0), (42, 0), (0, 1), (21, 1), (42, 1) };
+
+    public static int Combinations { get; private set; }
+    public static List<Result> Results { get; } = new();
+    private static int TotalComparer(Result a, Result b) => a.Total.CompareTo(b.Total);
+    private static int OperationsComparer(Result a, Result b) => a.Operations.CompareTo(b.Operations);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static bool IsValid(Op op, long x, long y) =>
+        op switch {
+            Op.Add => x <= y,
+            Op.Sub => x > y,
+            Op.Mul => x != 1 && y != 1 && x <= y,
+            Op.Div => (y > 1) & (x % y == 0),
+            _ => throw new ArgumentException("Operator not supported")
         };
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static bool IsValid(Op op, long x, long y) {
-            return op switch {
-                Op.Add => x <= y,
-                Op.Sub => x > y,
-                Op.Mul => x != 1 && y != 1 && x <= y,
-                Op.Div => y > 1 & ((x % y) == 0),
-                _ => throw new ArgumentException("Operator not supported")
-            };
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static Span<Result> GetSlice(int len) {
+        var slice = new Span<Result>(_memory, _offset, len);
+        _offset += len;
+        return slice;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void ReturnSlice(int len) => _offset -= len;
+
+    public static void Solve(List<long> numbers, long goal) {
+        _cache.Clear();
+        Combinations = 0;
+        var candidates = new Result[6];
+        for (var n = 0; n < numbers.Count; n++) {
+            candidates[n] = new ValRes(numbers[n]);
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static Span<Result> GetSlice(int len) {
-            var slice = new Span<Result>(_memory, _offset, len);
-            _offset += len;
-            return slice;
+        Array.Sort(candidates, TotalComparer);
+        Results.Clear();
+        SolveInternal(candidates, goal);
+        Results.Sort(OperationsComparer);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static Result Combine(Op op, Result x, Result y) {
+        Combinations++;
+        return new AppRes(op, x, y);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static Packed PackCandidates(Span<Result> candidates) {
+        var canLen = candidates.Length;
+        Span<long> nums = stackalloc long[2];
+        for (var c = 0; c < canLen; ++c) {
+            (var shift, var ix) = _packIndices[c];
+            nums[ix] |= candidates[c].Total << shift;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static void ReturnSlice(int len) {
-            _offset -= len;
+        return new Packed(nums[1], nums[0]);
+    }
+
+    private static void SolveInternal(Span<Result> candidates, long goal) {
+        var packed = PackCandidates(candidates);
+        if (_cache.Contains(packed)) {
+            return;
         }
 
-        public static void Solve(List<long> numbers, long goal) {
-            _cache.Clear();
-            Combinations = 0;
-            var candidates = new Result[6];
-            for (int n = 0; n < numbers.Count; n++)
-                candidates[n] = new ValRes(numbers[n]);
-            Array.Sort(candidates, TotalComparer);
-            Results.Clear();
-            SolveInternal(candidates, goal);
-            Results.Sort(OperationsComparer);
-        }
+        _cache.Add(packed);
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static Result Combine(Op op, Result x, Result y) {
-            Combinations++;
-            return new AppRes(op, x, y);
-        }
+        var canLen = candidates.Length;
+        for (var i = 0; i < canLen; ++i) {
+            for (var j = 0; j < canLen; ++j) {
+                if (i == j) {
+                    continue;
+                }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static UInt128 PackCandidates(Span<Result> candidates) {
-            int canLen = candidates.Length;
-            Span<long> nums = stackalloc long[2];
-            for (int c = 0; c < canLen; ++c) {
-                (int shift, int ix) = _packIndices[c];
-                nums[ix] |= candidates[c].Total << shift;
-            }
-            return new UInt128((ulong)nums[1], (ulong)nums[0]);
-        }
+                var x = candidates[i];
+                var y = candidates[j];
+                for (var k = 0; k < 4; ++k) {
+                    var op = _operations[k];
+                    if (!IsValid(op, x.Total, y.Total)) {
+                        continue;
+                    }
 
-        private static void SolveInternal(Span<Result> candidates, long goal) {
-
-            var packed = PackCandidates(candidates);
-            if (_cache.Contains(packed))
-                return;
-            _cache.Add(packed);
-
-            int canLen = candidates.Length;
-            for (int i = 0; i < canLen; ++i) {
-                for (int j = 0; j < canLen; ++j) {
-                    if (i == j) continue;
-                    var x = candidates[i];
-                    var y = candidates[j];
-                    for (int k = 0; k < 4; ++k) {
-                        var op = _operations[k];
-                        if (!IsValid(op, x.Total, y.Total))
-                            continue;
-
-                        var comb = Combine(op, x, y);
-                        if (comb.Total == goal) {
-                            Results.Add(comb);
-                        } else if (canLen > 2) {
-                            var rest = GetSlice(canLen - 1);
-                            bool placed = false;
-                            int r = 0;
-                            for (int l = 0; l < canLen; ++l) {
-                                if (l == i || l == j) continue;
-                                var can = candidates[l];
-                                if (!placed && can.Total >= comb.Total) {
-                                    rest[r++] = comb;
-                                    placed = true;
-                                }
-
-                                rest[r++] = can;
+                    var comb = Combine(op, x, y);
+                    if (comb.Total == goal) {
+                        Results.Add(comb);
+                    } else if (canLen > 2) {
+                        var rest = GetSlice(canLen - 1);
+                        var placed = false;
+                        var r = 0;
+                        for (var l = 0; l < canLen; ++l) {
+                            if (l == i || l == j) {
+                                continue;
                             }
 
-                            if (!placed) rest[r] = comb;
-                            SolveInternal(rest, goal);
-                            ReturnSlice(canLen - 1);
+                            var can = candidates[l];
+                            if (!placed && can.Total >= comb.Total) {
+                                rest[r++] = comb;
+                                placed = true;
+                            }
+
+                            rest[r++] = can;
                         }
+
+                        if (!placed) {
+                            rest[r] = comb;
+                        }
+
+                        SolveInternal(rest, goal);
+                        ReturnSlice(canLen - 1);
                     }
                 }
             }
